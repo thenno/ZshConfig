416310678de6501cb852c2ef74e27ae46120d771
Max Zuev <mail@thenno.me>
2021-08-13 18:35:09 +0300

    Disable autoupdate

--- oh-my-zsh/oh-my-zsh.sh
+++ oh-my-zsh/oh-my-zsh.sh
@@ -1,7 +1,5 @@
 # Check for updates on initial load...
-if [ "$DISABLE_AUTO_UPDATE" != "true" ]; then
-  /usr/bin/env ZSH=$ZSH DISABLE_UPDATE_PROMPT=$DISABLE_UPDATE_PROMPT zsh -f $ZSH/tools/check_for_upgrade.sh
-fi
+/usr/bin/env ZSH=$ZSH DISABLE_UPDATE_PROMPT=true zsh -f $ZSH/tools/check_for_upgrade.sh
 
 # Initializes Oh My Zsh
 
