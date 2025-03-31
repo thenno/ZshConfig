fcbfc133fa0eef58e5ae2e0661f639591abd4ecd
Maksim Zuev <chiffa@tracto.ai>
2025-03-31 18:02:25 +0200

    Return forgotten fixes to rg

--- plugins/rg/rg.plugin.zsh
+++ plugins/rg/rg.plugin.zsh
@@ -6,5 +6,5 @@ rg() {
     return 1
   fi
 
-  \grep -G --color=auto -R --exclude-dir={build,.git,.idea,.tox,.venv,.tools_venv,.ruff_cache,.mypy_cache,\*proto_stubs,__pycache__,\*.egg-info,.pytest_cache} --exclude={\*.pyc} $@ .
+  \grep -G --color=auto -R --exclude-dir={build,.git,.idea,.tox,.venv,.tools_venv,.ruff_cache,.mypy_cache,\*proto_stubs,__pycache__,\*.egg-info,.pytest_cache,build,dist} --exclude={\*.pyc} $@ .
 }
